b493b5c1579e8e7a3d1cd092763c74a92b7d3551
dwainetrain <dwaine.best@gmail.com>
2017-12-31 18:11:18 -0800

    Refactored tests to use Django Test Client. Cleaned up overly redundant tests

--- lists/tests.py
+++ lists/tests.py
@@ -1,22 +1,10 @@
-from django.urls import resolve
 from django.test import TestCase
-from lists.views import home_page
-from django.http import HttpRequest
-from django.template.loader import render_to_string
 
 class HomePageTest(TestCase):
 
     # How to test if app is installed?
 
-    def test_root_url_reolves_to_home_page_view(self):
-        found = resolve('/')
-        self.assertEqual(found.func, home_page)
-
     def test_home_page_returns_correct_html(self):
-        request = HttpRequest()
-        response = home_page(request)
-        html = response.content.decode('utf8')
-        expected_html = render_to_string('home.html')
-        self.assertEqual(html, expected_html)
-
+        response = self.client.get('/')
+        self.assertTemplateUsed(response, 'home.html')
 # Create your tests here.

d00c2db52734bb5ff2bee4d060cd64f647ab9fd3
dwainetrain <dwaine.best@gmail.com>
2018-01-05 07:14:29 -0800

    Refactored tests.

--- lists/views.py
+++ lists/views.py
@@ -13,4 +13,4 @@ def home_page(request):
 
 def view_list(request):
     items = Item.objects.all()
-    return render (request, 'home.html', {'items': items})
+    return render (request, 'index.html', {'items': items})

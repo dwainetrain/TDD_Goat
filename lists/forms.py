6536811d708aa16b30eaa5ae40c22692d40dba04
dwainetrain <dwaine.best@gmail.com>
2018-01-25 16:53:22 -0800

    Add forms and test_forms py files.

---
+++ lists/forms.py
@@ -0,0 +1,15 @@
+from django import forms
+
+from lists.models import Item
+
+class ItemForm(forms.models.ModelForm):
+    #  item_text = forms.CharField(
+    #      widget = forms.fields.TextInput(attrs={
+    #          'placeholder':'Enter a to-do item',
+    #          'class': 'form-control input-lg',
+    #      }),
+    # )
+
+    class Meta:
+        model = Item
+        fields = ('text',)

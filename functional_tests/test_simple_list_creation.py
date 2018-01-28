6e5b1a7ef9b4b24fce554b388029fe3d2046adc1
dwainetrain <dwaine.best@gmail.com>
2018-01-27 18:21:31 -0800

    fixed find-replace debacle. rename all item input ids and names. still broken.

--- functional_tests/test_simple_list_creation.py
+++ functional_tests/test_simple_list_creation.py
@@ -15,7 +15,7 @@ class NewVisitorTest(FunctionalTest):
         self.assertIn('To-Do', header_text)
 
         # He decides to give it a whirl
-        inputbox = self.browser.find_element_by_id('id_new_item')
+        inputbox = self.get_item_input_box()
         self.assertEqual(
             inputbox.get_attribute('placeholder'),
             'Enter a to-do item'
@@ -35,7 +35,7 @@ class NewVisitorTest(FunctionalTest):
 
         # The text entry remains, and he sees he can add more items to the todo List
         # He adds his next item "And then Some"
-        inputbox = self.browser.find_element_by_id('id_new_item')
+        inputbox = self.get_item_input_box()
         inputbox.send_keys('And then some')
         inputbox.send_keys(Keys.ENTER)
 
@@ -47,7 +47,7 @@ class NewVisitorTest(FunctionalTest):
     def test_multiple_users_can_start_lists_at_different_urls(self):
         # Jacob starts a new to-do lists
         self.browser.get(self.live_server_url)
-        inputbox = self.browser.find_element_by_id('id_new_item')
+        inputbox = self.get_item_input_box()
         inputbox.send_keys('Do Everything')
         inputbox.send_keys(Keys.ENTER)
         self.wait_for_row_in_list_table('1: Do Everything')
@@ -74,7 +74,7 @@ class NewVisitorTest(FunctionalTest):
 
         # Francis starts a new list by entering a new item. He
         # is less interesting than Edith...
-        inputbox = self.browser.find_element_by_id('id_new_item')
+        inputbox = self.get_item_input_box()
         inputbox.send_keys('Buy milk')
         inputbox.send_keys(Keys.ENTER)
         self.wait_for_row_in_list_table('1: Buy milk')

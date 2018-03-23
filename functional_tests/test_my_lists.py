2b6d0201d57d3b2d89f23414f057d0c75aa2a6f4
dwainetrain <dwaine.best@gmail.com>
2018-03-23 11:41:13 -0700

    testing if local authentication error also happens on staging site.

--- functional_tests/test_my_lists.py
+++ functional_tests/test_my_lists.py
@@ -49,7 +49,7 @@ class MyListsTest(FunctionalTest):
         self.wait_for(
             lambda: self.browser.find_element_by_link_text('Reticulate splines')
         )
-        self.broswer.find_element_by_link_text('Reticulate splines').click()
+        self.browser.find_element_by_link_text('Reticulate splines').click()
         self.wait_for(
             lambda: self.assertEqual(self.browser.current_url, first_list_url)
         )
